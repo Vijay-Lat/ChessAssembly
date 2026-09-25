9a8c78dc4e4c5c9119b13a1693cb49f5d6ca73fa
Vijay-Lat <vijayserpentine@gmail.com>
2026-09-26 00:10:15 +0530

    add array structure

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+import { ArrayStructure } from "./ArrayStructure.js";
+
 function helloWorld(){
     console.log("Implemented",this);
 }
-this.helloWorld();
+helloWorld();
 
 const whatIsThis = ()=>{
     const name = "Name"
@@ -9,3 +11,17 @@ const whatIsThis = ()=>{
 }
 
 whatIsThis();
+
+const array = new ArrayStructure();
+
+array.push("Hello");
+array.push("world");
+
+const getAr = array.get(1);
+array.push("lastItem");
+
+console.log("array before pop:", JSON.parse(JSON.stringify(array)));
+
+array.pop();
+
+console.log("array after pop:", JSON.parse(JSON.stringify(array)));
