cc9b7adbb196fed0d5fc201cf2372cd06ac73e4d
Vijay-Lat <vijayserpentine@gmail.com>
2026-09-25 08:14:56 +0530

    This Keyword Commit

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function helloWorld(){
     console.log("Implemented",this);
 }
 this.helloWorld();
+
+const whatIsThis = ()=>{
+    const name = "Name"
+    console.log(this,"What is this");
+}
+
+whatIsThis();
